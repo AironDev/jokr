f5fa03e224a76668b998911dc5384e2d7f113462
AironDev <airondev@gmail.com>
2020-09-15 23:29:51 +0100

    signup logic

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,9 +1,45 @@
+<!doctype html>
+<html class="no-js" lang="en">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
 
+    <!-- CSS
+    ============================================ -->
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/bootstrap.min.css')}}">
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/bicon.min.css')}}">
+    <!-- Flat Icon CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/flaticon.css')}}">
+    <!-- audio & video player CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/plyr.css')}}">
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/slick.min.css')}}">
+    <!-- nice-select CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/nice-select.css')}}">
+    <!-- perfect scrollbar css -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/perfect-scrollbar.css')}}">
+    <!-- light gallery css -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/lightgallery.min.css')}}">
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- Font-awesome -->
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/all.css') }}" rel="stylesheet" />
 
-@extends('layouts.guest')
+</head>
 
-@section('content')
+<body class="bg-transparent">
     <main>
         <div class="main-wrapper pb-0 mb-0">
             <div class="timeline-wrapper">
@@ -151,6 +187,7 @@
             </div>
         </div>
     </main>
+
 
     <!-- JS
 ============================================ -->
